6ff716d6ba12d82b5bb75276215f7616ae4661db
Benjamin Nicholas <ben@ben-nicholas.com>
2013-12-31 10:51:16 -0500

    Gracefully handle links with no href, resolves #3

--- lib/extractor.rb
+++ lib/extractor.rb
@@ -10,7 +10,7 @@ module Shelob
     def extract
       content = Nokogiri::HTML(@fetched.body)
       raw = content.css('a').map { |anchor| anchor['href'] }
-      raw.map do |link| 
+      raw.reject(&:nil?).map do |link| 
         if link.start_with? '/' 
           u = URI(@fetched.url)
           "#{u.scheme}://#{u.host}#{link}"
--- test/test_extractor.rb
+++ test/test_extractor.rb
@@ -15,8 +15,10 @@ describe Shelob::Extractor, "Link extracting module" do
     before do
       @result = LinkResult.new("http://google.com", 200, '<html><head><title>hi</title></head><body><a href="http://bing.com">bing</a><a href="http://yahoo.com">yahoo</a></body></html>')
       @result2 = LinkResult.new("http://google.com/something", 200, '<html><head><title>hi</title></head><body><a href="/about">about</a></body></html>')
+      @result3 = LinkResult.new("http://google.com/another", 200, '<html><head><title>hi</title></head><body><a>about</a><a href="http://boop.com">boop</a></body></html>')
       @le = Shelob::Extractor.new(@result)
       @le2 = Shelob::Extractor.new(@result2)
+      @le3 = Shelob::Extractor.new(@result3)
     end
 
     it "should return a list of the links in the page" do
@@ -29,6 +31,13 @@ describe Shelob::Extractor, "Link extracting module" do
       extracts = @le2.extract
       extracts.must_be_kind_of Array
       extracts.must_equal ["http://google.com/about"]
+    end
+
+    it "should gracefully handle empty links" do
+      # we shouldn't get an exception here
+      extracts = @le3.extract
+      extracts.must_be_kind_of Array
+      extracts.must_equal ["http://boop.com"]
     end
 
   end # describe
